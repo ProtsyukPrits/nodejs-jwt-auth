e102936e24a54d55c786b443803e61840219ff01
ProtsyukPrits <dmytro.protsiuk.prits@gmail.com>
2024-04-28 20:36:11 +0200

    created server pj, jwt-autentification, expres,cors,

---
+++ routes/api/employees.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllEmployees,
+  getEmployee,
+  createNewEmployee,
+  deleteEmployee,
+  updateEmployee,
+} = require("../../controllers/employeesController");
+
+router
+  .route("/")
+  .get(getAllEmployees)
+  .post(createNewEmployee)
+  .put(updateEmployee)
+  .delete(deleteEmployee);
+
+router.route("/:id").get(getEmployee);
+
+module.exports = router;
